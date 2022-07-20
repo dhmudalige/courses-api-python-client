4dd6c4bf3de63a6dd3a7aa2d780c7a215a1abe68
dhmudalige <e18227@eng.pdn.ac.lk>
2022-07-20 07:17:35 +0530

    Add my_course_code_example.py

---
+++ my_course_code_example.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+
+# create an instance of the API class
+api_instance = swagger_client.DefaultApi(swagger_client.ApiClient(configuration))
+# api_instance = swagger_client.DefaultApi(swagger_client.ApiClient(swagger_client.Configuration()))
+# api_instance = swagger_client.DefaultApi(swagger_client.ApiClient(configuration=swagger_client.Configuration(host='https://localhost')))
+semester = 'semester_example' # str | Available Semesters (1, 2, 3, 4, 5, 6, 7, 8, general, technical, short-semester)
+course_code = 'course_code_example' # str | Course Code
+
+try:
+    api_response = api_instance.get_course_by_code(semester, course_code)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling DefaultApi->get_course_by_code: %s\n" % e)
